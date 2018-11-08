2cbf6e7f842120d0e54ec54280bbe31ad8001f14
sarahgoossens <33448008+sarahgoossens@users.noreply.github.com>
2018-11-08 11:58:17 +0100

    Add files via upload
    Code zender en ontvanger xbee modules.
    Niet alle code is relevant, maar komt van voorbeelden voor meer duidelijkheid.

---
+++ Code/ontvanger_xbee.ino
@@ -0,0 +1,93 @@
+#include <XBee.h>
+#include <SoftwareSerial.h>
+
+XBee xbee = XBee();
+XBeeResponse response = XBeeResponse();
+
+uint8_t ssRX = 8;
+uint8_t ssTX = 9;
+SoftwareSerial nss(ssRX, ssTX);
+
+Rx16Response rx16 = Rx16Response();
+Rx64Response rx64 = Rx64Response();
+
+int statusLed = 13;
+
+uint8_t option = 0;
+uint8_t *data;
+uint8_t length;
+uint16_t zender;
+
+void flashLed(int times, int wait) {
+    
+    for (int i = 0; i < times; i++) {
+      digitalWrite(statusLed, HIGH);
+      delay(wait);
+      digitalWrite(statusLed, LOW);
+      
+      if (i + 1 < times) {
+        delay(wait);
+      }
+    }
+}
+
+void setup() {
+  pinMode(statusLed, OUTPUT);
+  Serial.begin(9600);
+
+  nss.begin(9600);
+  xbee.setSerial(nss);
+
+}
+
+// continuously reads packets, looking for RX16 or RX64
+void loop() {
+
+    xbee.readPacket();
+    
+    if (xbee.getResponse().isAvailable()) {
+      Serial.println("iets ontvangen");
+      flashLed(1,100);
+         
+      if (xbee.getResponse().getApiId() == RX_16_RESPONSE || xbee.getResponse().getApiId() == RX_64_RESPONSE) {
+       Serial.println("ontvangen bericht is rx bericht");
+        
+        if (xbee.getResponse().getApiId() == RX_16_RESPONSE) {
+            Serial.println("rx16 bericht");
+            xbee.getResponse().getRx16Response(rx16);
+            Serial.print("RSSI = ");
+            Serial.println( rx16.getRssi() );
+        	option = rx16.getOption();
+        	data = rx16.getData();
+          length = rx16.getDataLength();
+          zender = rx16.getRemoteAddress16();
+        }// else {
+//            Serial.println("rx64 bericht");
+//            xbee.getResponse().getRx64Response(rx64);
+//            option = rx64.getOption();
+//        	  data = rx64.getData();
+//            length = rx64.getDataLength();
+//            zender = rx64.getRemoteAddress64();
+//        }
+        Serial.print("option: ");
+        Serial.println(option);
+        Serial.print("zender: ");
+        Serial.println(zender);
+        Serial.print(", data: ");
+        for(int i = 0; i<length; i++)
+          Serial.print((char)data[i]);
+        Serial.println();
+
+      
+
+      } else {
+      	Serial.println("geen rx bericht");    
+      }
+    } else if (xbee.getResponse().isError()) {
+      Serial.println("Error reading packet.  Error code: ");  
+      Serial.println(xbee.getResponse().getErrorCode());
+    }
+
+
+    
+}
---
+++ Code/zender_xbee.ino
@@ -0,0 +1,83 @@
+#include <XBee.h>
+#include <SoftwareSerial.h>
+
+XBee xbee = XBee();
+ 
+uint8_t ssRX = 8;
+uint8_t ssTX = 9;
+SoftwareSerial nss(ssRX, ssTX);
+
+uint8_t payload[] = { 0, 0, 0, 0 };
+
+//master MY value=3005
+//slave MY value=1611
+Tx16Request tx = Tx16Request(0x1611, payload, sizeof(payload));
+
+TxStatusResponse txStatus = TxStatusResponse();
+
+int statusLed = 13;
+
+void flashLed(int times, int wait) {
+    
+    for (int i = 0; i < times; i++) {
+      digitalWrite(statusLed, HIGH);
+      delay(wait);
+      digitalWrite(statusLed, LOW);
+      
+      if (i + 1 < times) {
+        delay(wait);
+      }
+    }
+}
+
+void setup() {
+  nss.begin(9600);
+  xbee.setSerial(nss);
+
+  pinMode(statusLed, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+
+    payload[0] = 't';
+    payload[1] = 'e';
+    payload[2] = 's';
+    payload[3] = 't';
+    
+    xbee.send(tx);
+    Serial.println("Bericht verzonden");
+    flashLed(1, 100);
+    
+  
+    // after sending a tx request, we expect a status response
+    // wait up to 5 seconds for the status response
+    if (xbee.readPacket(5000)) {
+        // got a response!
+        Serial.println("Ontvangen bericht");
+      
+       
+       // should be a znet tx status            	
+    	if (xbee.getResponse().getApiId() == TX_STATUS_RESPONSE) {
+           Serial.println("is status respons");
+    	   xbee.getResponse().getTxStatusResponse(txStatus);
+    		
+    	   // get the delivery status, the fifth byte
+           if (txStatus.getStatus() == SUCCESS) {
+                Serial.println("status respons is success");
+             	flashLed(5, 50);
+             
+           } else {
+            	// the remote XBee did not receive our packet. is it powered on?
+             	Serial.println("status respons is niet succes, remote heeft bericht niet ontvangen");
+           }
+        }      
+    } else if (xbee.getResponse().isError()) {
+      Serial.println("Error reading packet.  Error code: ");  
+      Serial.println(xbee.getResponse().getErrorCode());
+    } else {
+      Serial.println("Error");
+    }
+    
+    delay(1000);
+}
